51ab4333edd9a56b1cbaa38eed5449e7b16602e1
Sergio Tejón <stejon@freepik.com>
2024-10-17 22:15:20 +0200

    5 check commit messages if they adhere to conventional commit standards (#6)
    * feat(internal/cmd/functions.go): Implement new function
    
    - Create a new empty file 'functions.go' in the internal/cmd directory
    - This will serve as a placeholder for new functions to be implemented in future commits
    
    * feat(internal/cmd/root.go): Implement noop mode and logging
    
    - Allow running the command without making any changes by adding a new flag
    - Add debug logging for commit message
    - Exit with success status if in noop mode
    - Commit and amend the changes when not in noop mode, as before
    - Update init function to handle new flags and modify Execute function accordingly
    
    * chore: Update yaml package version
    
    from v2 to v3 (gopkg.in/yaml.v3 v3.0.1 -> gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c)
    
    - Update package reference in go.mod file to match the new version
    - Replace old import statements with the new ones in the required files
    - Test the project functionality to ensure compatibility with the updated package

---
+++ internal/cmd/functions.go
@@ -0,0 +1,82 @@
+package cmd
+
+import (
+	"bytes"
+	"fmt"
+	"log/slog"
+	"strings"
+	"text/template"
+
+	"github.com/leodido/go-conventionalcommits"
+	"github.com/leodido/go-conventionalcommits/parser"
+
+	"github.com/sergiotejon/ai-commit/internal/ai"
+)
+
+// checkCommitMessage checks the commit message for conventional commits.
+func checkCommitMessage(message string) error {
+	_, err := parser.NewMachine(parser.WithTypes(conventionalcommits.TypesConventional)).Parse([]byte(message))
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// printCommitMessage prints the commit message.
+func printCommitMessage(message string) {
+	fmt.Printf("\nCommit message:\n\n%s\n\n", message)
+}
+
+// promptTemplate generates a prompt template using the diff.
+func generatePrompt(promptTemplate, diff string) (string, error) {
+	var tmpl, err = template.New("prompt").Parse(promptTemplate)
+	if err != nil {
+		return "", err
+	}
+
+	var tpl bytes.Buffer
+	err = tmpl.Execute(&tpl, struct{ Diff string }{Diff: diff})
+	if err != nil {
+		return "", err
+	}
+
+	return tpl.String(), nil
+}
+
+// generateCommitMessage generates a commit message using Ollama.
+func generateCommitMessage(prompt, ollamaServer, model string, retries int) (string, error) {
+	var result string
+	var cleanResult string
+	var err error
+
+	for i := 0; i < retries; i++ {
+		result, err = ai.QueryOllama(prompt, ollamaServer, model)
+		if err != nil {
+			// Retry if there is an error
+			slog.Warn("Failed to query Ollama", "retry", i, "error", err)
+			continue
+		}
+
+		cleanResult = strings.TrimSpace(strings.Map(func(r rune) rune {
+			if strings.ContainsRune("`", r) {
+				return -1
+			}
+			return r
+		}, result))
+
+		err = checkCommitMessage(cleanResult)
+		if err == nil {
+			// Valid commit message, exit the loop and continue
+			break
+		}
+		printCommitMessage(cleanResult)
+		slog.Error("Invalid commit message", "retry", i, "error", err)
+	}
+
+	if cleanResult == "" {
+		return "", fmt.Errorf("failed to generate a valid commit message")
+	}
+
+	return cleanResult, err
+}
