614ef5b24b528076f3fff283e575a1a033b68f27
Sergio Tejón <sergio.tejon@gmail.com>
2024-11-22 21:20:17 +0100

    refactor(internal/cmd/functions.go): Remove unused imports and functions, update generateCommitMessage function signature
    - Delete "bytes", "text/template", "fmt", "log/slog" imports
    - Delete "generatePrompt" function
    - Update generateCommitMessage function to accept OllamaOptions as an argument
    - Modify the loop structure in generateCommitMessage to use the new function signature
    - Update root.go to reflect changes in import section and flags usage

--- internal/cmd/root.go
+++ internal/cmd/root.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/spf13/cobra"
 
+	"github.com/sergiotejon/ai-commit/internal/ai"
 	"github.com/sergiotejon/ai-commit/internal/configure"
 	"github.com/sergiotejon/ai-commit/internal/git"
 	"github.com/sergiotejon/ai-commit/internal/logger"
@@ -51,19 +52,20 @@ var rootCmd = &cobra.Command{
 			os.Exit(1)
 		}
 
-		// Return prompt to infer
-		prompt, err := generatePrompt(string(configure.Cfg.DefaultPromptTemplate), diff)
-		if err != nil {
-			panic(err)
+		systemPrompt := string(*configure.Cfg.SystemPrompt)
+		options := ai.OllamaOptions{
+			NumCtx:      *configure.Cfg.NumCtx,
+			Temperature: *configure.Cfg.Temperature,
+			NumKeep:     *configure.Cfg.NumKeep,
 		}
 
-		slog.Debug("Prompt to infer", "prompt", prompt)
+		slog.Debug("Prompt to infer", "systemPrompt", systemPrompt, "prompt", diff, "options", options)
 
 		slog.Info("Querying Ollama...")
 		slog.Info("Parameters", "server", ollamaServer, "model", model)
 
 		// Generate the commit message using Ollama from the prompt with the differences
-		commitMessage, err := generateCommitMessage(prompt, ollamaServer, model, retries)
+		commitMessage, err := generateCommitMessage(diff, systemPrompt, ollamaServer, model, retries, options)
 		if err != nil {
 			panic(err)
 		}
@@ -95,11 +97,11 @@ func init() {
 		panic(err)
 	}
 
-	rootCmd.PersistentFlags().BoolVar(&noop, "noop", configure.Cfg.Noop, "Run without making any changes")
-	rootCmd.PersistentFlags().StringVarP(&logLevel, "logLevel", "l", configure.Cfg.LogLevel, "Set the logging level (debug, info, warn, error)")
-	rootCmd.PersistentFlags().StringVar(&ollamaServer, "server", configure.Cfg.OllamaServer, "Set the Ollama server URL")
-	rootCmd.PersistentFlags().StringVar(&model, "model", configure.Cfg.Model, "Set the model to use for AI generation")
-	rootCmd.PersistentFlags().IntVarP(&retries, "retries", "r", configure.Cfg.DefaultRetriesCommitMessage, "Set the number of retries for invalid commit messages")
+	rootCmd.PersistentFlags().BoolVar(&noop, "noop", *configure.Cfg.Noop, "Run without making any changes")
+	rootCmd.PersistentFlags().StringVarP(&logLevel, "logLevel", "l", *configure.Cfg.LogLevel, "Set the logging level (debug, info, warn, error)")
+	rootCmd.PersistentFlags().StringVar(&ollamaServer, "server", *configure.Cfg.OllamaServer, "Set the Ollama server URL")
+	rootCmd.PersistentFlags().StringVar(&model, "model", *configure.Cfg.Model, "Set the model to use for AI generation")
+	rootCmd.PersistentFlags().IntVarP(&retries, "retries", "r", *configure.Cfg.RetriesCommitMessage, "Set the number of retries for invalid commit messages")
 	rootCmd.PersistentFlags().BoolVarP(&showVersion, "version", "v", false, "Show the version of the application")
 	rootCmd.PersistentFlags().BoolVar(&quiet, "quiet", false, "Run in silent mode")
 }
