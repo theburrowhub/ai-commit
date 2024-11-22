9ec7d439f9befa0d3ddf3d6c93d599587af65421
Sergio Tejón <sergio.tejon@gmail.com>
2024-11-22 21:17:19 +0100

    refactor(internal/configure/literalstring.go): Update import and remove unused package
    - Remove unnecessary import "gopkg.in/yaml.v3"
    - Move the remaining import statement to a new line for better readability
    - Update the code structure to follow the recommended style guide
    - Add missing blank lines for improved readability
    - Refactor the LoadConfig function to load and update the config file in one step
    - Set default values for new fields in the Config struct if they are nil
    - Remove unnecessary comments and unused variables
    - Update the createConfigFile function to set default values for new fields
    - Update the loadAndUpdateConfigFile function to handle cases where the config file already exists
    - Add a check for whether the config file was created or updated at the end of the createConfigFile function

--- internal/configure/configure.go
+++ internal/configure/configure.go
@@ -4,61 +4,51 @@ import (
 	"gopkg.in/yaml.v3"
 	"log/slog"
 	"os"
-	"path/filepath"
 )
 
 // Default values for the configuration
-const (
-	defaultNoopValue            = false
+var (
 	defaultLogLevel             = "info"
 	defaultModel                = "mistral"
 	defaultServer               = "http://localhost:11434"
 	defaultRetriesCommitMessage = 3
-	defaultPromptTemplate       = `Commit changes:
-{{ .Diff }}
+	defaultSystemPrompt         = "In an impersonal way, write a commit message that explains what the commit\n" +
+		"is for. Use conventional commits and the imperative mood in the first line.\n" +
+		"The first line should start with: feat, fix, refactor, docs, style, build,\n" +
+		"perf, ci, style, test or chore. Set the file name and the changes made in\n" +
+		"the body. Only one subject line is allowed.\n\n" +
+		"An example of commit message is:\n\n" +
+		"feat(file or class): Add user authentication\n\n" +
+		"- Implement user sign-up and login functionality\n" +
+		"- Add password hashing for security\n" +
+		"- Integrate with authentication API\n\n" +
+		"Add line breaks to separate subject from body."
+	defaultNumCtx      = 4096
+	defaultTemperature = float32(0)
+	defaultNumKeep     = 512
 
-In an impersonal way, write a commit message that explains what the commit is for. Use conventional commits
-and the imperative mood in the first line. The first line should start with: feat, fix, refactor, docs, style,
-build, perf, ci, style, test or chore. Set the file name and the changes made in the body. Only one subject
-line is allowed. An example of commit message is:
-
-feat(file or class): Add user authentication
-
-- Implement user sign-up and login functionality
-- Add password hashing for security
-- Integrate with authentication API
-
-Add line breaks to separate subject from body.`
+	configFilePath = os.Getenv("HOME") + "/.config/ai-commit/config.yaml" // Path to the configuration file
 )
 
 // Config represents the configuration for the ai-commit service
 type Config struct {
-	Noop                        bool          `yaml:"noop,omitempty"`
-	LogLevel                    string        `yaml:"logLevel,omitempty"`
-	OllamaServer                string        `yaml:"ollamaServer,omitempty"`
-	Model                       string        `yaml:"model,omitempty"`
-	DefaultPromptTemplate       LiteralString `yaml:"defaultPromptTemplate,omitempty"`
-	DefaultRetriesCommitMessage int           `yaml:"defaultRetriesCommitMessage,omitempty"`
+	Noop                 *bool          `yaml:"noop,omitempty"`
+	LogLevel             *string        `yaml:"logLevel,omitempty"`
+	OllamaServer         *string        `yaml:"ollamaServer,omitempty"`
+	Model                *string        `yaml:"model,omitempty"`
+	SystemPrompt         *LiteralString `yaml:"systemPrompt,omitempty"`
+	RetriesCommitMessage *int           `yaml:"retriesCommitMessage,omitempty"`
+	NumCtx               *int           `yaml:"numCtx,omitempty"`
+	NumKeep              *int           `yaml:"numKeep,omitempty"`
+	Temperature          *float32       `yaml:"temperature,omitempty"`
 }
 
 // Cfg represents the configuration for the ai-commit service
 var Cfg Config
 
-var (
-	configFilePath = os.Getenv("HOME") + "/.config/ai-commit/config.yaml" // Path to the configuration file
-)
-
 // LoadConfig loads the configuration
 func LoadConfig() error {
-	if _, err := os.Stat(configFilePath); os.IsNotExist(err) {
-		slog.Info("Config file not found. Creating config file...")
-		err := createConfigFile()
-		if err != nil {
-			return err
-		}
-	}
-
-	err := loadConfigFile()
+	err := loadAndUpdateConfigFile()
 	if err != nil {
 		return err
 	}
@@ -66,8 +56,16 @@ func LoadConfig() error {
 	return nil
 }
 
-// loadConfigFile loads the configuration from a yaml file
-func loadConfigFile() error {
+// loadAndUpdateConfigFile updates the config file with new fields and removes deprecated fields
+func loadAndUpdateConfigFile() error {
+	if _, err := os.Stat(configFilePath); os.IsNotExist(err) {
+		slog.Info("Config file not found. Creating config file...")
+		err = os.WriteFile(configFilePath, []byte{}, 0644)
+		if err != nil {
+			return err
+		}
+	}
+
 	data, err := os.ReadFile(configFilePath)
 	if err != nil {
 		return err
@@ -78,39 +76,45 @@ func loadConfigFile() error {
 		return err
 	}
 
+	// Set default values for new fields
+	setSomeDefaultValue := false
+
+	setSomeDefaultValue = setDefault(&Cfg.Noop, false)
+	setSomeDefaultValue = setDefault(&Cfg.LogLevel, defaultLogLevel) || setSomeDefaultValue
+	setSomeDefaultValue = setDefault(&Cfg.OllamaServer, defaultServer) || setSomeDefaultValue
+	setSomeDefaultValue = setDefault(&Cfg.Model, defaultModel) || setSomeDefaultValue
+	setSomeDefaultValue = setDefault(&Cfg.RetriesCommitMessage, defaultRetriesCommitMessage) || setSomeDefaultValue
+	setSomeDefaultValue = setDefault(&Cfg.SystemPrompt, LiteralString(defaultSystemPrompt)) || setSomeDefaultValue
+	setSomeDefaultValue = setDefault(&Cfg.NumCtx, defaultNumCtx) || setSomeDefaultValue
+	setSomeDefaultValue = setDefault(&Cfg.NumKeep, defaultNumKeep) || setSomeDefaultValue
+	setSomeDefaultValue = setDefault(&Cfg.Temperature, defaultTemperature) || setSomeDefaultValue
+
+	if setSomeDefaultValue {
+		updatedData, err := yaml.Marshal(&Cfg)
+		if err != nil {
+			return err
+		}
+
+		err = os.WriteFile(configFilePath, updatedData, 0644)
+		if err != nil {
+			return err
+		}
+
+		slog.Info("Config file updated successfully.")
+	}
+
 	return nil
 }
 
-// createConfigFile creates a default config file
-func createConfigFile() error {
-	defaultConfig := Config{
-		Noop:                        defaultNoopValue,
-		LogLevel:                    defaultLogLevel,
-		OllamaServer:                defaultServer,
-		Model:                       defaultModel,
-		DefaultRetriesCommitMessage: defaultRetriesCommitMessage,
-		DefaultPromptTemplate:       LiteralString(defaultPromptTemplate),
+// setDefault sets the default value for a field if it is nil
+// It returns true if the field was set to the default value
+func setDefault[T any](field **T, defaultValue T) bool {
+	if *field == nil {
+		*field = new(T)
+		**field = defaultValue
+
+		return true
 	}
 
-	data, err := yaml.Marshal(&defaultConfig)
-	if err != nil {
-		return err
-	}
-
-	if _, err := os.Stat(configFilePath); os.IsNotExist(err) {
-		err := os.MkdirAll(filepath.Dir(configFilePath), 0755)
-		if err != nil {
-			return err
-		}
-		err = os.WriteFile(configFilePath, data, 0644)
-		if err != nil {
-			return err
-		} else {
-			slog.Info("Config file created successfully.")
-		}
-	} else {
-		slog.Warn("Config file already exists.")
-	}
-
-	return nil
+	return false
 }
--- internal/configure/literalstring.go
+++ internal/configure/literalstring.go
@@ -1,15 +1,19 @@
 package configure
 
-import "gopkg.in/yaml.v3"
+import (
+	"gopkg.in/yaml.v3"
+)
 
 // LiteralString is a custom type to represent a string that should be serialized as a literal block.
 type LiteralString string
 
 // MarshalYAML implements the yaml.Marshaler interface for LiteralString.
 func (s LiteralString) MarshalYAML() (interface{}, error) {
-	return &yaml.Node{
+	node := &yaml.Node{
 		Kind:  yaml.ScalarNode,
+		Tag:   "!!str",
 		Value: string(s),
 		Style: yaml.LiteralStyle, // Uses the pipe style
-	}, nil
+	}
+	return node, nil
 }
